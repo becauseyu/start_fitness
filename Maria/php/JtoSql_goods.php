6efd1efc1446b3b3841a7d8a9c4eb210adcb3a15
becauseyu <becauseyu520@gmail.com>
2022-07-14 09:22:50 +0800

    0714

--- Maria/php/JtoSql_goods.php
+++ Maria/php/JtoSql_goods.php
@@ -9,18 +9,18 @@ $data1 = json_decode($JSON,true);
 $data2 = json_decode($JSON,false);
 // var_dump($data2);
 
-$sql="INSERT INTO goodsdetail(ptype,blendid,pstyle,pname,pcount,ppic,pprice) 
+$sql="INSERT INTO goodsdetail(ptype,bid,pstyle,pname,pcount,ppic,pprice) 
         VALUES (?,?,?,?,?,?,?)";
 
 $count='100';
 
 
 foreach($data2 as $row){
-    $sql_brand = "SELECT blid FROM blenddetail WHERE blname = '{$row->brand}'";
+    $sql_brand = "SELECT bid FROM branddetail WHERE bname = '{$row->brand}'";
     $result = $mysqli->query($sql_brand);
     $data =  $result->fetch_array(); //確認是否有符合的
     $stmt = $mysqli ->prepare($sql);
-    $stmt ->bind_param('sssssss',$row->type,$data['blid'],$row->style,$row->name,$count,$row->picture,$row->price);
+    $stmt ->bind_param('sssssss',$row->type,$data['bid'],$row->style,$row->name,$count,$row->picture,$row->price);
     //execute() 執行準備好的語句(prepare + parm)
     $stmt->execute();
     };
