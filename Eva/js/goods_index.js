8223e0f63cccd4543e96e85ca71957ef0492f868
Eva <Eva.tsai@gmail.com>
2022-07-17 11:17:57 +0800

    0717 1117

--- Eva/js/goods_index.js
+++ Eva/js/goods_index.js
@@ -1,47 +1,53 @@
 $('.footerpage').load('/MengYing/大專/LAB/footer.html')
+
 // 飲食的hover
-$(function () {
-  var itemnum = 0;
-  itemnum = $('.food').length;
 
-  //建立　換圖function
-  function menu(i) {
-    $('.food').eq(i).hover(
-      function () {
-        $(this).attr("src", "../asset/saleitem/food/0" + i + ".1.webp");
-      },
-      function () {
-        $(this).attr("src", "../asset/saleitem/food/0" + i + ".webp")
-      })
-  };
-  // 執行　function
-  for (i = 0; i < itemnum; i++) {
-    menu(i);
-  }
 
-})
+
+
+
+// $(function () {
+//   var itemnum = 0;
+//   itemnum = $('.food').length;
+
+//   //建立　換圖function
+//   function menu(i) {
+//     $('.food').eq(i).hover(
+//       function () {
+//         $(this).attr("src", "../asset/saleitem/food/0" + i + ".1.webp");
+//       },
+//       function () {
+//         $(this).attr("src", "../asset/saleitem/food/0" + i + ".webp")
+//       })
+//   };
+//   // 執行　function
+//   for (i = 0; i < itemnum; i++) {
+//     menu(i);
+//   }
+
+// })
 
 // 動動的hover
-$(function () {
-  var itemnum = 0;
-  itemnum = $('.gym').length;
+// $(function () {
+//   var itemnum = 0;
+//   itemnum = $('.gym').length;
 
-  //建立　換圖function
-  function menu(i) {
-    $('.gym').eq(i).hover(
-      function () {
-        $(this).attr("src", "../asset/saleitem/gym/0" + i + ".1.webp")
-      },
-      function () {
-        $(this).attr("src", "../asset/saleitem/gym/0" + i + ".webp")
-      })
-  };
-  // 執行　function
-  for (i = 0; i < itemnum; i++) {
-    menu(i);
-  }
+//   //建立　換圖function
+//   function menu(i) {
+//     $('.gym').eq(i).hover(
+//       function () {
+//         $(this).attr("src", "../asset/saleitem/gym/0" + i + ".1.webp")
+//       },
+//       function () {
+//         $(this).attr("src", "../asset/saleitem/gym/0" + i + ".webp")
+//       })
+//   };
+//   // 執行　function
+//   for (i = 0; i < itemnum; i++) {
+//     menu(i);
+//   }
 
-})
+// })
 
 
 
