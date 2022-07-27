9242e2cc34cb301d83c4ecd241245a57fd606112
WennyLai <n86064262@gs.ncku.edu.tw>
2022-07-27 10:49:46 +0800

    0727 圖標

--- start_fitness/resources/views/ld/goods/list.blade.php
+++ start_fitness/resources/views/ld/goods/list.blade.php
@@ -55,31 +55,34 @@
                                 <div class="col-3 text-center bname">{{ $goods->branddetail->bname }}</div>
                                 <div class="col-2 text-center "><img height="100" src="{{ $goods->url }}"></div>
                                 <div class="col-3 text-center button_bag">
-                                    <div class="mr-1 btn01 btn-outline01" onclick="edit('a{{ $goods->pid }}')">編輯</div> |
+                                    <div class="mr-1 btn01 btn-outline01" onclick="bigEdit('a{{ $goods->pid }}')">編輯</div>
+                                    |
                                     <div class="ml-1 btn01 btn-outline02">刪除</div>
                                 </div>
                             </div>
                         </button>
                     </div>
                 </form>
-                <form id="a{{ $flavor->pid }}" action="/flavor/edit" enctype="multipart/form-data" method="POST">
-                    @csrf
-                    <div id="collapse{{ $goods->pid }}" class="collapse body" data-parent="#accordionExample">
-                        <div class="card-body">
-                            <table class="table table-striped">
-                                <tr>
-                                    <th> 流水號 </th>
-                                    <th> 口味 </th>
-                                    <th> 數量 </th>
-                                    <th> 圖片 </th>
-                                    <th> 價格 </th>
-                                    <th>
-                                        <a href="Create.html" class="btn01 btn-outline" style="color: black ;">
-                                            新增口味
-                                        </a>
-                                    </th>
-                                </tr>
-                                @foreach ($goods->flavor as $flavor)
+                <div id="collapse{{ $goods->pid }}" class="collapse body" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <table class="table table-striped">
+                            <tr>
+                                <th> 流水號 </th>
+                                <th> 口味 </th>
+                                <th> 數量 </th>
+                                <th> 圖片 </th>
+                                <th> 價格 </th>
+                                <th>
+                                    <a href="Create.html" class="btn01 btn-outline" style="color: black ;">
+                                        新增口味
+                                    </a>
+                                </th>
+                            </tr>
+                            @foreach ($goods->flavor as $flavor)
+                                <form id="a{{ $flavor->pid }}" action="/flavor/edit" enctype="multipart/form-data"
+                                    method="POST">
+                                    @csrf
+
                                     <tr>
                                         <th class="pid"> {{ $flavor->pid }} </th>
                                         <th class="pstyle"> {{ $flavor->pstyle }} </th>
@@ -87,17 +90,19 @@
                                         <th> <img height="150" src="{{ $flavor->url }}"> </th>
                                         <th> $ <span class="pprice">{{ $flavor->pprice }}</span> </th>
                                         <th>
-                                            <div class="mr-1 btn01 btn-outline01" onclick="edit('a{{ $goods->pid }}')">編輯
+                                            <div class="mr-1 btn01 btn-outline01"
+                                                onclick="smaillEdit('a{{ $goods->pid }}')">編輯
                                             </div> |
                                             <div class="ml-1 btn01 btn-outline02">刪除</div>
                                         </th>
                                     </tr>
-                                @endforeach
+                                </form>
+                            @endforeach
 
-                            </table>
-                        </div>
+                        </table>
                     </div>
-                </form>
+                </div>
+
             </div>
             </form>
         @endforeach
@@ -110,7 +115,127 @@
 
 @section('script')
     <script>
-        function edit(id) {
+        // 大標題修改
+        function bigEdit(id) {
+            // 取得品牌資料
+            var brandList_url = window.location.origin + '/ld/branddetail/list';
+            var brandList = getList(brandList_url);
+
+            // 取得大分類資料
+            var ptypeList_url = window.location.origin + '/ld/ptype/list';
+            var ptypeList = getList(ptypeList_url);
+
+
+            // 先取得目標表單
+            getForm = document.getElementById(id);
+
+            // 鎖住原本的表格不要亂動
+            getForm.querySelector('.headButton').setAttribute("data-target", '');
+
+
+            // 所有可輸入值置換成input
+
+            // ptype----------------
+            ptype = getForm.querySelector('.ptype');
+            ptype_html = `<select name='ptype' value = '${ptype.innerText}'>`;
+            ptypeList.forEach((option) => {
+                if (ptype.innerText == option.ptype) {
+                    ptype_html += `<option value='${option.ptype}' selected>${option.ptype}</option>`;
+                } else {
+                    ptype_html += `<option value='${option.ptype}' >${option.ptype}</option>`;
+                }
+
+            })
+            ptype_html += `</select>`;
+
+            ptype.innerHTML = ptype_html;
+
+            // pname----------------
+            pname = getForm.querySelector('.pname');
+            pname.innerHTML = `<input name='pname' value ='${pname.innerText}'>`;
+            // pid------------------
+            getForm.querySelectorAll('.pid').forEach((pid) => {
+                pid.innerHTML = `<input name='pid' value ='${pid.innerText}' disabled>`;
+            })
+
+
+            // bname
+            bname = getForm.querySelector('.bname');
+            bname_html = `<select name='bid' value = '${bname.innerText}'>`;
+            brandList.forEach((option) => {
+                if (bname.innerText == option.bname) {
+                    bname_html += `<option value='${option.bid}' selected>${option.bname}</option>`;
+                } else {
+                    bname_html += `<option value='${option.bid}'>${option.bname}</option>`;
+                }
+
+            })
+            bname_html += `</select>`;
+            bname.innerHTML = bname_html;
+
+
+            // pstyle
+            getForm.querySelectorAll('.pstyle').forEach((pstyle) => {
+                pstyle.innerHTML = `<input name='pstyle[]' value ='${pstyle.innerText}'>`;
+            })
+
+
+            // pcount
+            getForm.querySelectorAll('.pcount').forEach((pcount) => {
+                pcount.innerHTML = `<input name='pcount[]' value ='${pcount.innerText}'>`;
+            })
+
+
+            // pprice
+            getForm.querySelectorAll('.pprice').forEach((pprice) => {
+                pprice.innerHTML = `<input name='pprice[]' value ='${pprice.innerText}'>`;
+            })
+
+
+            // button_bag
+            getForm.querySelector('.button_bag').innerHTML = `
+                <input type="submit" class="mr-1 btn01 btn-outline01" value='完成'></div> |
+                <a href='/ld/goods/list' class="ml-1 btn01 btn-outline02">取消編輯</a>
+            `
+
+
+
+
+            // pid ptype pname bname pstyle pcount pprice
+
+
+
+            // getAllInput.forEach(($element)=>{
+            //     $element.innerHTML = `<input value ='${$element.innerHTML}'' ></input>`
+            // } )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // 其他不相干表單隱藏
+            getAllForm = document.querySelectorAll(`form`);
+            getAllForm.forEach(($element) => {
+                if ($element.id != id) {
+                    console.log($element);
+                    $element.style.visibility = 'hidden';
+                    $element.style.position = 'fixed';
+                }
+            })
+        }
+
+
+        function smaillEdit(id) {
             // 取得品牌資料
             var brandList_url = window.location.origin + '/ld/branddetail/list';
             var brandList = getList(brandList_url);
@@ -229,7 +354,13 @@
             })
         }
 
-        // 取得表單資料
+
+
+
+
+
+
+        // xttp請求取得表單資料
         function getList(url) {
             var xhttp = new XMLHttpRequest();
             //如果當xhttp發生改變時，發生後面的callback(回乎函式) //閉包
