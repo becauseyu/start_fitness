56e4e8c82ffab6a488c6e31ffa7f96b7a81d955a
japan1426123 <japan1426123@gmail.com>
2022-07-28 11:01:44 +0800

    0728 調整兩側按鈕

--- start_fitness/resources/views/front_side_frame/sidebar_left.blade.php
+++ start_fitness/resources/views/front_side_frame/sidebar_left.blade.php
@@ -4,5 +4,13 @@
     </div> -->
     <!-- l側邊鈕/運動 -->
     <div id="mySidenavl" class="sidenavl">
-        <a href="/sport/index" id="Sidenavl"><img class="my-auto sideImg1 " src="/image/SP _bw.png" alt=""></a>
+
+        <a href="/sport/index" id="Sidenavl" class="">
+
+            <img class="my-auto sideImg1 " src="/image/SP _bw.png" alt="">
+            <div class=" float-right iconcenter1  my-auto">
+                <i class="fa fa-chevron-circle-right fa-3x   " aria-hidden="true"></i>
+            </div>
+
+        </a>
     </div>
