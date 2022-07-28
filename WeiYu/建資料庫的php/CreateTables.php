6c93c8b90797bcd3cfc960662ebd8f5032bce3b3
WennyLai <n86064262@gs.ncku.edu.tw>
2022-07-28 10:40:57 +0800

    0728

--- WeiYu/建資料庫的php/CreateTables.php
+++ WeiYu/建資料庫的php/CreateTables.php
@@ -138,7 +138,7 @@ function create_goodsdetail_table($mysqli) {
     `pcount` INT NOT NULL,
     `ppic` VARCHAR(128) NOT NULL,
     `pprice` INT NOT NULL ,
-    `staid` SMALLINT NOT NULL DEFAULT 0,
+    `staid` SMALLINT NOT NULL DEFAULT 1,
     PRIMARY KEY (`pid`)) ENGINE = InnoDB;";
     $result = $mysqli->query($sql);
     if ($result) { echo 'table goodsdetail is created';}else{echo 'Have error when create table goodsdetail, or it is exist';};
