c0b56979b31439c5a6fd23dec1cd906dd16b4a54
WennyLai <n86064262@gs.ncku.edu.tw>
2022-08-01 10:41:04 +0800

    0801 假會員產生器

--- WeiYu/建資料庫的php/a20220702_sql_functions.php
+++ WeiYu/建資料庫的php/a20220702_sql_functions.php
@@ -1,7 +1,7 @@
 <?php 
 
 // 新創會員
-function insertMember($name , $email , $tel , $account , $password , $point = 0, $staId = '1') {
+function insertMember($name , $email , $tel , $account , $password , $point = 0, $staId = 1) {
     include('mysql.php');
     $sql = "INSERT INTO member (`account`, `psw`, `name`, `email`, `tel`, `point`, `staId` ) VALUES (?,?,?,?,?,?,?);";
     $stmt = $mysqli->prepare($sql);
@@ -9,15 +9,6 @@ function insertMember($name , $email , $tel , $account , $password , $point = 0,
     $stmt->execute();
 
     echo 'member insert complete';
-
-    $sql = "INSERT INTO `log` (body) VALUES (?);";
-    $stmt = $mysqli->prepare($sql);
-    $stmt->bind_param('s',"新增會員，帳號 : ".$account);
-    $stmt->execute();
-
-
-
-
     return;
 }
    
