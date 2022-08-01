9159ec3c1d3bc54f292bbe358ef0e45b749a9657
WennyLai <n86064262@gs.ncku.edu.tw>
2022-08-01 11:25:18 +0800

    0801 取消log.txt連動

--- WeiYu/測試區/trigger/a0712test_trigger.php
+++ WeiYu/測試區/trigger/a0712test_trigger.php
@@ -10,32 +10,32 @@ BEGIN
 END $$
 DELIMITER ;
 
-// 2. 會員修該
+// // 2. 會員修該
 
-DELIMITER $$
-CREATE TRIGGER tr_log_userinfo_update
-after update ON member FOR EACH ROW
-BEGIN
-    SET @body = concat('會員資料被修改，帳號 : ', old.account);
-    INSERT INTO log(body) VALUES(@body);
-end $$
-DELIMITER ;
-
-
-// 3. 會員不能被更動
-
-DELIMITER $$
-CREATE TRIGGER tr_log_userinfo_no_update_id_account
-before update ON member FOR EACH ROW
-BEGIN
-    if  (old.id <> new.id) or (old.account <> new.account) or (old.permission <> new.permission) then
-    signal sqlstate '45001' set message_text = '會員帳號及id不能被修改';
-end if;
-end $$
-DELIMITER ;
+// DELIMITER $$
+// CREATE TRIGGER tr_log_userinfo_update
+// after update ON member FOR EACH ROW
+// BEGIN
+//     SET @body = concat('會員資料被修改，帳號 : ', old.account);
+//     INSERT INTO log(body) VALUES(@body);
+// end $$
+// DELIMITER ;
 
 
-// 4. 
+// // 3. 會員不能被更動
+
+// DELIMITER $$
+// CREATE TRIGGER tr_log_userinfo_no_update_id_account
+// before update ON member FOR EACH ROW
+// BEGIN
+//     if  (old.id <> new.id) or (old.account <> new.account) or (old.permission <> new.permission) then
+//     signal sqlstate '45001' set message_text = '會員帳號及id不能被修改';
+// end if;
+// end $$
+// DELIMITER ;
+
+
+// // 4. 
 
 
 
